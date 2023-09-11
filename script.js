0f5833dfb44bf275f891743f19b132444010d1c9
Ayush Tiwari <01tiwari.ayush@gmail.com>
2023-09-11 21:13:38 +0530

    removed bugs

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 const createBtn = document.querySelector(".create-btn");
 const notesContainer = document.querySelector(".notes-container");
-const inputBox = document.querySelector(".input-box");
+
+const note = document.querySelector(".note");
 
 createBtn.addEventListener("click", function () {
   const div = document.createElement("div");
-  const p = document.createElement("p");
-  const i = document.createElement("i");
   div.className = "note";
-  i.className = "fa-solid fa-trash dlt-btn";
+
+  const p = document.createElement("p");
   p.setAttribute("contenteditable", "true");
   p.className = "input-box";
   div.appendChild(p);
+
+  const i = document.createElement("i");
+  i.className = "fa-solid fa-trash dlt-btn";
   div.append(i);
+
   notesContainer.insertBefore(div, notesContainer.firstChild);
 
-  i.addEventListener("click", function () {
-    div.remove();
-  });
+  //   saveData();
 });
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "I") {
+    console.log(1);
+    e.target.parentElement.remove();
+    saveData();
+  } else {
+    const inputBox = document.querySelectorAll(".input-box");
+    inputBox.forEach((x) => {
+      x.onkeyup = saveData;
+    });
+  }
+});
+
+// const dltBtn = document.querySelectorAll(".dlt-btn");
+
+function saveData() {
+  const data = notesContainer.innerHTML;
+  localStorage.setItem("data", data);
+}
+function getData() {
+  const data = localStorage.getItem("data");
+  notesContainer.innerHTML = data;
+}
+getData();
